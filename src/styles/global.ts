326e5fc0ef5e36a3f017a4a8744c9191931b86a6
bruna15lima <calheiros.bruna@gmail.com>
2021-05-20 20:34:02 -0300

    ok

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,8 +4,8 @@ export const GlobalStyle = createGlobalStyle`
 :root {
   --background: #f0f2f5;
   --red: #E52E4D;
+  --green: #33CC95;
   --blue: #5429CC;
-
   --blue-ligth: #6933FF;
   --text-title: #363F5F;
   --text-body: #969CB3;
