29ca5876645eb700687eef6286359e4ddf202953
bruna15lima <calheiros.bruna@gmail.com>
2021-05-20 22:43:56 -0300

    modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
+import Modal from 'react-modal';
 import { Container, Content } from './styles'
 
 
 export function Header() {
+  const [isNewTransictionModalOpen, setIsNewTransictionModalOpen] = useState(false);
+  
+  function handleOpenNewTransactionModal() {
+    setIsNewTransictionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransictionModalOpen(false);
+  }
+
+
   return(
     <Container>  
       <Content>
       <img src={logoImg} alt="dtmoney"/>
-      <button type="button">
+      <button type="button" onClick={handleOpenNewTransactionModal}>
         Nova transação
       </button>
+ 
+      <Modal isOpen={isNewTransictionModalOpen}>
+        <h2>Cadastrat Transação</h2>
+      </Modal>
       </Content>
     </Container>
   )
